6ef65d7849725a07bd719df0af11def08196d5a5
Mitsushidu <fireminergoodplay@gmail.com>
2023-09-30 19:20:54 +0300

    add docker, add api views, add api urlss

--- usetech/api/views.py
+++ usetech/api/views.py
@@ -2,13 +2,14 @@ from authentication.models import Service, Client
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+import jwt
 
 
 @api_view(['GET'])
 def authentication(request):
     print(request.headers['Service'])
     if request.headers['Service'] == 'bomba':
-        return Response(status=status.HTTP_200_OK)
+       return Response(status=status.HTTP_200_OK)
     return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
