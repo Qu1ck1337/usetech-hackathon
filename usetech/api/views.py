6e0aaec20b536fbfb03b6b97c1903fe0803c8cc6
Mitsushidu <fireminergoodplay@gmail.com>
2023-10-01 08:27:44 +0300

    123

--- usetech/api/views.py
+++ usetech/api/views.py
@@ -1,4 +1,5 @@
 from authentication.models import Service, Client
+import requests
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -7,13 +8,17 @@ import jwt
 
 @api_view(['GET'])
 def authentication(request):
-    print(request.headers['Service'])
-    if request.headers['Service'] == 'bomba':
-       return Response(status=status.HTTP_200_OK)
-    return Response(status=status.HTTP_401_UNAUTHORIZED)
+    public_key = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAukToMbbOi6ntGRYuSzcVRZiv2hI/mSZ3sT3huUT33v8x6XgB7HGw37irfdaFBcN5Rbxtb9/cDdOHM0hKyKNKvUktsqjPTnaGe8CDJUGtLWamLgGBYAabKyln1tMxFi2+qV9FU87IOit213/HYz/pEfQNl+z2C9ht+mitpB/BmyakOPhVov9oa0g7aVLrMaAfVmaadeGjDxAvFvHFI9vdaLhu50Q1Dyr3gVbnC5xIT9o3GV3Lh3O/EntvxUaytLyeZPfQ3jv50gWZePuaGFdlm+1Av4Od2Fw7NAO+xCnOltROrJBqylGDV/I5/LfAJC3YF3e3kQv/SqCKLY4l5Cx08QIDAQAB'
+    public_key_data = '-----BEGIN PUBLIC KEY-----\n' + public_key + '\n-----END PUBLIC KEY-----'
+    jwt_token = request.headers['JWT']
+    try:
+        payload = jwt.decode(jwt_token, key=public_key_data, algorithms=['RS256'], audience='account')
+        print(payload["client_id"])
+    except Exception:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+    return Response(status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
 def service1(request):
-    print(request.META.get('HTTP_service'))
     return Response(status=status.HTTP_200_OK)
